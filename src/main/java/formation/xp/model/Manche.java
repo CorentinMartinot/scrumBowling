26876d26331815b4dd7858aa550c51542989d4c7
corentinML <corentin.martinot@gmail.com>
2019-12-13 15:57:55 +0100

    play Partie

--- src/main/java/formation/xp/model/Manche.java
+++ src/main/java/formation/xp/model/Manche.java
@@ -1,4 +1,39 @@
 package formation.xp.model;
 
+import java.util.List;
+
 public class Manche {
+    private int number;
+    private List<Tour> listeTour;
+
+    Manche(int number){
+        number=this.number;
+    }
+
+    public void initManche(List<Joueur> listeJoueur){
+        for (Joueur joueur : listeJoueur) {
+            Tour tour = new Tour(joueur,this.maxLancé());
+            listeTour.add(tour);
+        }
+    }
+
+    public int play(){
+        for (Tour tour : listeTour){
+            tour.play();
+            tour.print();
+        }
+
+
+        return 0;
+    }
+
+
+    public int maxLancé(){
+        if (this.number==10) {
+            return 3;
+        } else {
+            return 2;
+        }
+    }
+
 }

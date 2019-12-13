261c838bb07841b25d2d52280e967d7411c2a9e1
Ju <julien.gribal@gmail.com>
2019-12-13 11:33:29 +0100

    commit nbJoueur nouvelle classe

---
+++ src/main/java/formation/xp/model/Partie.java
@@ -0,0 +1,21 @@
+package formation.xp.model;
+
+public class Partie {
+
+    private int nbJoueur;
+
+    public Partie(){
+
+    }
+
+    //Getters
+    public int getnbJoueur(){
+        return this.nbJoueur;
+    }
+
+    //Setters
+    public void setNbJoueur(int nbJoueur){
+        this.nbJoueur=nbJoueur;
+    }
+
+}
